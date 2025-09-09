b0535e9886c551ea9b6a64432f2924fa715e17a3
nagashimam <masato.ngsm@gmail.com>
2025-09-09 14:54:51 +0900

    fix: Errors on exit

--- src/worker.ts
+++ src/worker.ts
@@ -4,9 +4,15 @@ import { run } from "./run.js";
 
 parentPort?.on(
 	"message",
-	async (message: { templatePath: string; variation: HtmlVariation }) => {
-		const { templatePath, variation } = message;
-		const result = await run(templatePath, variation);
-		parentPort?.postMessage(result);
+	async (
+		message: { templatePath: string; variation: HtmlVariation } | "close",
+	) => {
+		if (message === "close") {
+			parentPort?.close();
+		} else {
+			const { templatePath, variation } = message;
+			const result = await run(templatePath, variation);
+			parentPort?.postMessage(result);
+		}
 	},
 );
